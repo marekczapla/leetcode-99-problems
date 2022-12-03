cd5736b007d82f8cca33192ee98f3b9003d1d9d3
marekczapla <106765325+marekczapla@users.noreply.github.com>
2022-12-03 22:27:10 +0100

    10/99

--- src/pl/markopolo/Main.java
+++ src/pl/markopolo/Main.java
@@ -2,8 +2,6 @@ package pl.markopolo;
 
 public class Main {
     public static void main(String[] args) {
-        SingleNumber number = new SingleNumber();
-        System.out.println(number.singleNumber(new int[] {1,2,3,4,1,3,3,3,3}));
 
 
     }
